eaf4a6912a2c75c4deb65d323ef1b69554880dac
KnockbackNemo <madeleinercflee@gmail.com>
2026-04-08 13:16:07 -0500

    validation_4 is ready for a homopolymer-seeking run! It will probably fail, but oh well- let's try it!

--- nnsight_patch_validation_3.py
+++ nnsight_patch_validation_3.py
@@ -55,38 +55,49 @@ first_read = next(reads)
 
 
 # Chop data
-start_idx = 54500 # Skip beginning noise
-LENGTH = 400
+start_idx = 54250 # Skip beginning noise
+LENGTH = 250
 end_idx = start_idx + LENGTH
 raw_stndrd_signal = first_read.signal
 chopped_signal = raw_stndrd_signal[start_idx:end_idx]
 
 clean_signal = chopped_signal.copy()
-corrupt_signal = clean_signal.copy()
+
 
 # Make fake signal spike
-SPIKE_LEN = 8 # Even
-SPIKE_MIDPOINT = 240
-SPIKE_SURROUND_WIDTH = 50
+# SPIKE_LEN = 8 # Even
+# SPIKE_MIDPOINT = 240
+# SPIKE_SURROUND_WIDTH = 50
+# NUM_TO_REPLACE = 0
+REPLACE_START = 130
+# steal_base_start = 150 
+steal_base_len = 6 # Must be even
+steal_base_spike_start = 100
 
+# part_to_copy = clean_signal[steal_base_start:steal_base_start + steal_base_len]
+# part_to_copy = part_to_copy - 0.75*part_to_copy + part_to_copy.mean()
+part_for_spike = clean_signal[steal_base_spike_start:steal_base_spike_start + steal_base_len]
 
 # Flatten the data around the spike area to make it look like a plateau
-corrupt_signal[SPIKE_MIDPOINT - SPIKE_SURROUND_WIDTH : SPIKE_MIDPOINT + SPIKE_SURROUND_WIDTH] *= 0.1 
-corrupt_signal[SPIKE_MIDPOINT - SPIKE_SURROUND_WIDTH : SPIKE_MIDPOINT + SPIKE_SURROUND_WIDTH] -= .40 
-clean_signal[SPIKE_MIDPOINT - SPIKE_SURROUND_WIDTH : SPIKE_MIDPOINT + SPIKE_SURROUND_WIDTH] *= 0.1
-clean_signal[SPIKE_MIDPOINT - SPIKE_SURROUND_WIDTH : SPIKE_MIDPOINT + SPIKE_SURROUND_WIDTH] -= 0.40
+
 
 # Add spike
-spike = np.linspace(-3, 3, SPIKE_LEN)
-spike = -np.exp(-0.5 * spike**2) * .50
-corrupt_signal[SPIKE_MIDPOINT - int(SPIKE_LEN/2) : SPIKE_MIDPOINT + int(SPIKE_LEN/2)] += spike
+# spike = np.linspace(-3, 3, SPIKE_LEN)
+# spike = -np.exp(-0.5 * spike**2) * .50
+# corrupt_signal[SPIKE_MIDPOINT - int(SPIKE_LEN/2) : SPIKE_MIDPOINT + int(SPIKE_LEN/2)] += spike
+# corrupt_signal[SPIKE_MIDPOINT - int(steal_base_len/2): SPIKE_MIDPOINT + int(steal_base_len/2)] = clean_signal[steal_base_start:steal_base_start+steal_base_len]
 
+# Modify data to add a homopolymer and a spike
+
+# clean_signal[REPLACE_START:REPLACE_START + NUM_TO_REPLACE*steal_base_len] = np.tile(part_to_copy, NUM_TO_REPLACE)
+corrupt_signal = clean_signal.copy()
+corrupt_signal[REPLACE_START : REPLACE_START + steal_base_len] = part_for_spike
 
 
 # Move signal to tensors and GPU
 clean_input = torch.tensor(clean_signal, dtype=model_dtype).view(1, 1, LENGTH).to(model.device)
 corrupted_input = torch.tensor(corrupt_signal, dtype=model_dtype).view(1, 1, LENGTH).to(model.device)
-
+first_input = torch.tensor(chopped_signal, dtype=model_dtype).view(1, 1, LENGTH).to(model.device)
 
 # Let's see if the output suggests A->C and A->G homopolymers
 with model.trace(clean_input):
@@ -101,6 +112,10 @@ with model.trace(corrupted_input):
     patched_output = model.output.save()
 
 
+with model.trace(first_input):
+    first_output = model.output.save()
+
+
 file_name, extension = os.path.splitext(__file__)
 
 
@@ -108,6 +123,7 @@ file_name, extension = os.path.splitext(__file__)
 string_clean = bonito_model.decode(clean_output[:, 0, :]) # Need to convert to a numpy array in memory
 string_corrupted = bonito_model.decode(corrupted_output[:, 0, :]) 
 string_patched = bonito_model.decode(patched_output[:, 0, :]) 
+string_orig = bonito_model.decode(first_output[:, 0, :]) 
 
 
 
@@ -115,6 +131,7 @@ with open(f"{file_name}_strings.txt", "w") as f:
     print(f"Clean output string: {string_clean}", file=f)
     print(f"Corrupt output strn: {string_corrupted}", file=f)
     print(f"Patched output strn: {string_patched}", file=f)
+    print(f"Origin output strng: {string_orig}", file=f)
 
 # Check signal
 plt.figure()
