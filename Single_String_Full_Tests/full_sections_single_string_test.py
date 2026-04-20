98ad845a11716ddbd8a114c71199909e81a33106
KnockbackNemo <madeleinercflee@gmail.com>
2026-04-20 11:50:08 -0500

    Add more metrics for posterior probabilities as well! So much data.

--- Single_String_Full_Tests/full_sections_single_string_test.py
+++ Single_String_Full_Tests/full_sections_single_string_test.py
@@ -59,8 +59,6 @@ def run_patching_sweep(model, source_input, target_input, check_output_timestamp
 
     # Each row is a patch (differ in layer #, component type, & timestep)
     results = []
-
-    # heatmap_data = [np.zeros((NUM_T_LAYERS, NUM_TIMESTEP_SWEEPS)) for _ in range(3)]
     
     ######## Get baseline clean and corrupt outputs for comparison #######
     with model.trace(source_input):
@@ -125,10 +123,6 @@ def run_patching_sweep(model, source_input, target_input, check_output_timestamp
                     target_reshaped = target.clone().reshape(-1, nhead, head_dim)
                     src_reshaped = src_act.reshape(-1, nhead, head_dim)
                     
-                    # start = max(0, t - 1)
-                    # end = min(seq_len, t + 2)
-
-
                     target_reshaped[start:end, head_idx, :] = src_reshaped[start:end, head_idx, :]
 
                     patched = target_reshaped.reshape(seq_len, d_model)
@@ -136,52 +130,59 @@ def run_patching_sweep(model, source_input, target_input, check_output_timestamp
                     # Reshape and put back into activations
                     layer.self_attn.out_proj.input = patched 
 
-                    # ## Sanity check
-                    # orig = target.clone()
-                    # diff = (patched - orig).abs()
-                    # # reshape both the same way you do for heads
-                    # diff_heads = diff.reshape(seq_len, nhead, head_dim)
-                    # # sum per head
-                    # head_magnitudes = diff_heads.sum(dim=(0, 2))  # [nhead]
-                    # print("Head diffs:", head_magnitudes)
-
                 patched_scores_proxy = model.output.save()
 
             patched_scores = patched_scores_proxy.detach()
 
             ####### Calculate metrics ##########
             for timestep in check_output_timestamps_list:
-                
-                target_transition_idx = torch.argmax(source_output[timestep, 0, :]).item()
-                wrong_transition_idx = torch.argmax(target_output[timestep, 0, :]).item()
+                target_transition_idx = torch.argmax(source_output[timestep, 0, :]).item() # Note that "target" now means correct instead of "corrupted string"
+                wrong_transition_idx = torch.argmax(target_output[timestep, 0, :]).item() 
                 logit_diff_source = source_output[timestep,0,target_transition_idx] - source_output[timestep,0,wrong_transition_idx]
                 logit_diff_target = target_output[timestep,0,target_transition_idx] - target_output[timestep,0,wrong_transition_idx]
                 baseline_diff = logit_diff_source - logit_diff_target
 
 
-                # time_idx = check_output_timestamps_list[i].timestamp
-                # target_idx = check_output_timestamps_list[i].target_transition
-                # wrong_idx = check_output_timestamps_list[i].wrong_transition
-
                 patched_diff = patched_scores[timestep, 0, target_transition_idx] - patched_scores[timestep, 0, wrong_transition_idx]
                 if baseline_diff == 0:
                     score = 0
                 if metric_mode == "recovery":
-                    score = 1 - (logit_diff_source - patched_diff) / baseline_diff
+                    score = 1 - (logit_diff_source - patched_diff) / baseline_diff # 1 means full recovery, 0 means no change
                     score = score.item()
                 elif metric_mode == "degradation":
-                    score = (logit_diff_source - patched_diff) / baseline_diff
+                    score = (logit_diff_source - patched_diff) / baseline_diff # 1 means full degradation, 0 means no change
                     score = score.item()
 
-                # Get additional metrics
+                ### Get additional metrics ###
+                # Logits and strings
                 target_logit = patched_scores[timestep, 0, target_transition_idx].item()
                 wrong_logit = patched_scores[timestep, 0, wrong_transition_idx].item()
-                global_mse_diff = torch.nn.functional.mse_loss(source_output.to(torch.float32), patched_scores.to(torch.float32)).item()
-                actual_max_pred = torch.argmax(patched_scores[timestep, 0, :]).item()
-                
+                actual_max_pred_idx = torch.argmax(patched_scores[timestep, 0, :]).item()
+                target_string = transition_idx_to_str(target_transition_idx)
+                wrong_string = transition_idx_to_str(wrong_transition_idx)
+                actual_string = transition_idx_to_str(actual_max_pred_idx)
+
+                # Posteriors and strings - this includes global most likely path calculations
+                source_posteriors = bonito_model.seqdist.posteriors(source_output.to(torch.float32)) + 1e-8
+                target_posteriors = bonito_model.seqdist.posteriors(target_output.to(torch.float32)) + 1e-8
+                patched_posteriors = bonito_model.seqdist.posteriors(patched_scores.to(torch.float32)) + 1e-8
+                posteriors_target_transition_idx = torch.argmax(source_posteriors[timestep, 0, :]).item()
+                posteriors_wrong_transition_idx = torch.argmax(target_posteriors[timestep, 0, :]).item()
+                posteriors_actual_transition_idx = torch.argmax(patched_posteriors[timestep, 0, :]).item()
+                target_state_prob = patched_posteriors[timestep, 0, posteriors_target_transition_idx]
+                wrong_state_prob = patched_posteriors[timestep, 0, posteriors_wrong_transition_idx]
+                actual_state_prob = patched_posteriors[timestep, 0, posteriors_actual_transition_idx]
+                posteriors_target_string = transition_idx_to_str(posteriors_target_transition_idx)
+                posteriors_wrong_string = transition_idx_to_str(posteriors_wrong_transition_idx)
+                posteriors_actual_string = transition_idx_to_str(posteriors_actual_transition_idx)                
+
+
+
                 # If the MSE is greater than clean vs corrupt or the target prediction for this particular timestep isn't correct, decode and save the string
-                is_target_argmax = actual_max_pred == target_transition_idx
-                should_decode = is_target_argmax or (global_mse_diff > global_mse_baseline_diff)
+                is_target_logit_argmax = actual_max_pred_idx == target_transition_idx
+                is_target_prob_argmax = posteriors_actual_transition_idx == posteriors_target_transition_idx
+                global_mse_diff = torch.nn.functional.mse_loss(source_output.to(torch.float32), patched_scores.to(torch.float32)).item()
+                should_decode = not is_target_logit_argmax or not is_target_prob_argmax or (global_mse_diff > global_mse_baseline_diff)
 
                 string = None
                 if should_decode:
@@ -195,16 +196,26 @@ def run_patching_sweep(model, source_input, target_input, check_output_timestamp
                     "Component": component if head_idx is None else f"head_{head_idx}",
                     "Metric_Mode": metric_mode,
                     "Score": score,
-                    "Target": target_transition_idx,
-                    "Target_String": bonito_model.seqdist.path_to_str([target_transition_idx]),
+                    "Target_Transition_Prior": target_transition_idx,
+                    "Target_String_Prior": target_string,
                     "Target_Logit": target_logit,
-                    "Wrong_Transition": wrong_transition_idx,
-                    "Wrong_String": bonito_model.seqdist.path_to_str([wrong_transition_idx]),
+                    "Wrong_Transition_Prior": wrong_transition_idx,
+                    "Wrong_String_Prior": wrong_string,
                     "Wrong_Logit": wrong_logit,
-                    "Actual_Argmax": actual_max_pred,
-                    "Actual_String": bonito_model.seqdist.path_to_str([actual_max_pred]),
-                    "Actual_Argmax_Logit": patched_scores[timestep, 0, actual_max_pred],
-                    "Is_Target_Argmax": is_target_argmax,
+                    "Actual_Argmax_Prior": actual_max_pred_idx,
+                    "Actual_String_Prior": actual_string,
+                    "Actual_Argmax_Logit": patched_scores[timestep, 0, actual_max_pred_idx],
+                    "Target_Transition_Post": posteriors_target_transition_idx,
+                    "Target_String_Post": posteriors_target_string,
+                    "Target_Prob": target_state_prob,
+                    "Wrong_Transition_Post": posteriors_wrong_transition_idx,
+                    "Wrong_String_Post": posteriors_wrong_string,
+                    "Wrong_Prob": wrong_state_prob,
+                    "Actual_Transition_Post": posteriors_actual_transition_idx,
+                    "actual_String_Post": posteriors_actual_string,
+                    "actual_Prob": actual_state_prob,
+                    "Is_Target_Logit_Argmax": is_target_logit_argmax,
+                    "Is_Target_Prob_Argmax": is_target_prob_argmax,
                     "Should_Decode": should_decode,
                     "Global_MSE_Change": global_mse_diff - global_mse_baseline_diff,
                     "New_String": string
@@ -230,7 +241,7 @@ def plot_and_save_outputs(df, component="mlp"):
             csv_filename = (f"{component}_data_step_{step}.csv")
             csv_full_path = os.path.join(folder_path, csv_filename) 
 
-            png_filename = f"{file_name}_{component}_heatmap_results_stp_{step}.png"
+            png_filename = (f"{component}_heatmap_results_stp_{step}.png")
             png_full_path = os.path.join(folder_path, png_filename) 
 
             os.makedirs(folder_path, exist_ok=True)
@@ -356,38 +367,6 @@ PATCHING_SWEEP_WINDOW_END_IDX = 16#time_to_transformer_idx(LENGTH)#REPLACE_START
 NUM_TIMESTEP_SWEEPS = int((PATCHING_SWEEP_WINDOW_END_IDX - PATCHING_SWEEP_WINDOW_START_IDX) / 1) # TODO Later: divide by timestep patch size
 
 
-# Metrics to look at:
-# Correct transition vs wrong transition
-# Sum of C states - sum of blank states
-# later - Sum of A states?
-#       - Sum of blank states?
-###### Things because I can't think right now but I 100% need to fix this (hardcode 22-24)
-# TARGET_TRANSITIONS = [torch.argmax(clean_output[22, 0, :]).item(), torch.argmax(clean_output[23, 0, :]).item(), torch.argmax(clean_output[24, 0, :]).item()]
-
-# WRONG_TRANSITIONS = [torch.argmax(corrupted_output[22, 0, :]).item(), torch.argmax(corrupted_output[23, 0, :]).item(), torch.argmax(corrupted_output[24, 0, :]).item()]
-# timestep_requests = {}
-
-# heatmap_data_sum_diff_23 = np.zeros((NUM_T_LAYERS, NUM_TIMESTEP_SWEEPS)) # test all three
-# heatmap_data_sum_diff_24 = np.zeros((NUM_T_LAYERS, NUM_TIMESTEP_SWEEPS))
-# heatmap_data_sum_diff_22 = np.zeros((NUM_T_LAYERS, NUM_TIMESTEP_SWEEPS))
-# heatmap_data_argmax_diffs = [np.zeros((NUM_T_LAYERS, NUM_TIMESTEP_SWEEPS)), np.zeros((NUM_T_LAYERS, NUM_TIMESTEP_SWEEPS)), np.zeros((NUM_T_LAYERS, NUM_TIMESTEP_SWEEPS))]
-
-# patching_strings = {}
-######################################################################
-
-# # Logit difference timestep constants
-# logit_diffs_clean= []
-# logit_diffs_corrupt= []
-# for i in range(0, len(TARGET_TRANSITIONS)):
-#     logit_diffs_clean.append(clean_output[22 + i, 0, TARGET_TRANSITIONS[i]] - clean_output[22 + i, 0, WRONG_TRANSITIONS[i]])
-#     logit_diffs_corrupt.append(corrupted_output[22 + i, 0, TARGET_TRANSITIONS[i]] - corrupted_output[22 + i, 0, WRONG_TRANSITIONS[i]])
-
-# baseline_diffs = []
-# for i in range(0, len(TARGET_TRANSITIONS)): # Positive if clean is closer to the target than wrong, negative if corrupt is closer to target than wrong, 0 if same
-#     baseline_diffs.append(logit_diffs_clean[i].to(torch.float32) - logit_diffs_corrupt[i].to(torch.float32))
-# print(f"Output shape: {clean_output.shape}")
-# print(f"baseline diff: {baseline_diffs}")
-
 ##############################
 ###### PATCHING SWEEP ########
 ##############################
@@ -428,17 +407,4 @@ plot_and_save_outputs(attn_recovery, component="attn_denoising")
 plot_and_save_outputs(mlp_degradation, component="mlp_noising")
 plot_and_save_outputs(attn_degradation, component="attn_noising")
 ### plot_and_save_outputs(head_degradations, component="head_noising")
-
-
-
-# with open(f"{file_name}_strings.txt", "w") as f:
-#     print(f"Clean output string: {string_clean}", file=f)
-#     print(f"Corrupt output strn: {string_corrupted}", file=f)
-#     print()
-
-#     # Loop through heatmap and print each string
-#     for layer_idx in range(NUM_T_LAYERS):
-#         print(f"--- Patched Strings for Layer {layer_idx} ---", file=f)
-#         for time_offset, t in enumerate(range(SWEEP_WINDOW_START, SWEEP_WINDOW_END)):
-#             print(f"Time {time_offset} : {patching_strings[layer_idx, time_offset]} : scores: {heatmap_data_argmax_diffs[0][layer_idx, time_offset]}, {heatmap_data_argmax_diffs[1][layer_idx, time_offset]}, {heatmap_data_argmax_diffs[2][layer_idx, time_offset]}", file=f)
 
